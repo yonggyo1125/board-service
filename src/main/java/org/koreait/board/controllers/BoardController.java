8030bb9425391341db6ca35eeba6a7393c6bae39
yonggyo1125 <yonggyo00@kakao.com>
2025-01-21 18:23:21 +0900

    .

--- src/main/java/org/koreait/board/controllers/BoardController.java
+++ src/main/java/org/koreait/board/controllers/BoardController.java
@@ -1,9 +1,69 @@
 package org.koreait.board.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.koreait.global.rests.JSONData;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
+    /**
+     * 게시판 설정 한개 조회
+     *
+     * @param bid
+     * @return
+     */
+    @GetMapping("/config/{bid}")
+    public JSONData config(@PathVariable("bid") String bid) {
+
+        return null;
+    }
+
+    /**
+     * 게시글 등록, 수정 처리
+     *
+     * @return
+     */
+    @PostMapping("/save")
+    public JSONData save() {
+
+        return null;
+    }
+
+    /**
+     * 게시글 한개 조회
+     * - 글보기, 글 수정시에 활용될 수 있음(프론트앤드)
+     *
+     * @param seq
+     * @return
+     */
+    @GetMapping("/view/{seq}")
+    public JSONData view(@PathVariable("seq") Long seq) {
+
+        return null;
+    }
+
+    /**
+     * 게시글 목록 조회
+     *
+     * @param bid
+     * @return
+     */
+    @GetMapping("/list/{bid}")
+    public JSONData list(@PathVariable("bid") String bid) {
+
+        return null;
+    }
+
+    /**
+     * 게시글 한개 삭제
+     *
+     * @param seq
+     * @return
+     */
+    @DeleteMapping("/{seq}")
+    public JSONData delete(@PathVariable("seq") Long seq) {
+
+        return null;
+    }
 }
--- src/main/java/org/koreait/board/controllers/CommentController.java
+++ src/main/java/org/koreait/board/controllers/CommentController.java
@@ -1,11 +1,59 @@
 package org.koreait.board.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.koreait.global.rests.JSONData;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/comment")
 public class CommentController {
+
+    /**
+     * 댓글 작성, 수정
+     *
+     * @return
+     */
+    @PostMapping("/save")
+    public JSONData save() {
+
+        return null;
+    }
+
+    /**
+     * 댓글 한개 조회
+     *  - 댓글 수정시 기초 데이터(프론트앤드)
+     *
+     * @param seq
+     * @return
+     */
+    @GetMapping("/view/{seq}")
+    public JSONData view(@PathVariable("seq") Long seq) {
+
+        return null;
+    }
+
+    /**
+     * 댓글 목록 조회
+     *
+     * @param seq : 게시글 번호
+     * @return
+     */
+    @GetMapping("/list/{seq}")
+    public JSONData list(@PathVariable("seq") Long seq) {
+
+        return null;
+    }
+
+    /**
+     * 댓글 한개 삭제
+     *
+     * @param seq
+     * @return
+     */
+    @DeleteMapping("/{seq}")
+    public JSONData delete(@PathVariable("seq") Long seq) {
+
+        return null;
+    }
 }
