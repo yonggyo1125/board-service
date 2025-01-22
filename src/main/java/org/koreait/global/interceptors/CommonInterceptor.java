3e1d8facfb6178f6434af142a5015d1e44faaf5a
yonggyo1125 <yonggyo00@kakao.com>
2025-01-22 18:20:31 +0900

    .

---
+++ src/main/java/org/koreait/global/interceptors/CommonInterceptor.java
@@ -0,0 +1,16 @@
+package org.koreait.global.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class CommonInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return true;
+    }
+}
