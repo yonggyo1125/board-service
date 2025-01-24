33582e972f80dc3d9bfc23f73d6c246e6ea2465b
yonggyo1125 <yonggyo00@kakao.com>
2025-01-24 11:50:42 +0900

    .

--- src/main/java/org/koreait/global/configs/SwaggerConfig.java
+++ src/main/java/org/koreait/global/configs/SwaggerConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info=@Info(title="파일 API"))
+@OpenAPIDefinition(info=@Info(title="게시판 API"))
 public class SwaggerConfig {
     
     @Bean
     public GroupedOpenApi apiGroup() {
         return GroupedOpenApi.builder()
-                .group("파일 API v1")
+                .group("게시판 API v1")
                 .pathsToMatch("/**")
                 .build();
     }
